89e89d2424e33d9684a402e3fc6fa75500a3b41a
shlomif <shlomif@27f4d2be-69b7-b1af-7dc8-71755b09f50e>
2009-04-13 18:34:38 +0000

    Removed more code from Qt

--- hitori-solver-in-ruby/gui/qt/hitori-gui.rb
+++ hitori-solver-in-ruby/gui/qt/hitori-gui.rb
@@ -19,9 +19,6 @@ class CannonField < Qt::Widget
     def initialize(parent, hitori)
         super(parent)
         @hitori = hitori
-        @shootAngle = 0
-        @shootForce = 0
-        @target = Qt::Point.new(0, 0)
         @gameEnded = false
         @barrelPressed = false
         setPalette( Qt::Palette.new( Qt::Color.new( 250, 250, 200) ) )
@@ -43,7 +40,6 @@ class CannonField < Qt::Widget
             midnight = Qt::Time.new( 0, 0, 0 )
             srand( midnight.secsTo(Qt::Time.currentTime()) )
         end
-        @target = Qt::Point.new( 200 + rand(190), 10  + rand(255) )
         update()
     end
     
@@ -117,30 +113,6 @@ class CannonField < Qt::Widget
         end
         
         painter.end
-    end
-
-    def shotRect()
-        gravity = 4.0
-
-        velocity  = @shootForce
-        radians   = @shootAngle*3.14159265/180.0
-
-        velx      = velocity*cos( radians )
-        vely      = velocity*sin( radians )
-        x0        = ( @barrelRect.right()  + 5.0 )*cos(radians)
-        y0        = ( @barrelRect.right()  + 5.0 )*sin(radians)
-        x         = x0 + velx*time
-        y         = y0 + vely*time - 0.5*gravity*time*time
-
-        r = Qt::Rect.new( 0, 0, 6, 6 );
-        r.moveCenter( Qt::Point.new( x.round, height() - 1 - y.round ) )
-        return r
-    end
-
-    def targetRect()
-        r = Qt::Rect.new( 0, 0, 20, 10 )
-        r.moveCenter( Qt::Point.new(@target.x(),height() - 1 - @target.y()) )
-        return r
     end
 end
 
